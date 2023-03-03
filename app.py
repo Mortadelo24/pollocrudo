61bc395f71222540bb97a3be8156423ec2b83df4
Mortadelo24 <josedanielmirandaperez410@gmail.com>
2023-03-03 14:49:13 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# python -m venv env //crear un entorno
+# inclopeto la activacion del entorno
+
+
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, scoped_session
+from dotenv import load_dotenv
+load_dotenv()
+
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL no esta disponible")
+
+# set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+nombre= "Juan"
+apellido= 'Larios'
+query = text("INSERT INTO registro(nombre, apellidos) VALUES(:nombre, :apellidos)")
+
+db.execute(query, {'nombre': nombre, 'apellidos': apellido})
+
+db.commit()
